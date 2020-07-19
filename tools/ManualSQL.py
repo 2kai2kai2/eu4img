e83836df6f954249fec4acc5f3b09b1ab558f41e
2kai2kai2 <2kai2kai2@gmail.com>
2020-07-18 22:36:08 -0700

    Created manual SQL access tool

---
+++ tools/ManualSQL.py
@@ -0,0 +1,26 @@
+from typing import List, Optional, Union, Tuple
+import psycopg2
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+conn: psycopg2.extensions.connection = psycopg2.connect(database=getenv("database"), user=getenv("user"), password=getenv("password"), host=getenv("host"), port=getenv("port"))
+conn.autocommit = True
+
+cur: psycopg2.extensions.cursor = conn.cursor()
+while True:
+	command = input("> ")
+	if command == "QUIT":
+		cur.close()
+		conn.close()
+		quit()
+	else:
+		try:
+			cur.execute(command)
+			try:
+				print(cur.fetchall())
+			except ProgrammingError:
+				pass
+		except Exception as e:
+			print(repr(e))
