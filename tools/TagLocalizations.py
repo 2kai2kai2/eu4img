3c4961ad3215995027f3493d4ec7cb2da368508d
2kai2kai2 <2kai2kai2@gmail.com>
2020-07-18 19:21:17 -0700

    Improved Tag Localizations
    Wrote a script to go through localization files and find tags. Will add alternative names for "The" or accents. May include other localizations that look like tags but are not really. Also puts tags in alphabetical order.
    
    Updated localization using the new program (and a bit of manual filtering of non-nation tag lookalikes) to fix issues with some tags not being included.

---
+++ tools/TagLocalizations.py
@@ -0,0 +1,45 @@
+from typing import List, Optional, Union, Tuple
+from os import path, listdir
+import unicodedata
+
+language = "english"
+directory = input("Directory: ")
+
+# Fix directory when dragging file from explorer into VS code terminal
+if directory.startswith("&"):
+	directory = directory.strip("& '")
+
+# Check if directory exists
+if not path.exists(directory):
+	print("Path not found.")
+	quit()
+else:
+	print("Path found. Searching...")
+
+# Find the files we need
+allFiles = listdir(directory)
+specFiles: List[str] = [f for f in allFiles if path.isfile(directory+"\\"+f) and language in f]
+
+# Compile all the lines from all the files
+allLines: List[str] = []
+
+for f in specFiles:
+	fileObj = open(directory + "\\" + f, "r", encoding="utf8")
+	allLines.extend(fileObj.readlines())
+	fileObj.close()
+	del(fileObj)
+
+# Find only the ones that are nation tags
+tagLines: List[str] = [line.strip() for line in allLines if len(line) > 5 and line[0] == " " and line[4] == ":" and line[:5] == line[:5].upper()]
+tagLines.sort()
+
+def deaccent(string: str) -> str:
+	return str(unicodedata.normalize('NFKD', string)).encode('ASCII', 'ignore').decode('ASCII')
+
+for line in tagLines:
+	name = line[line.index('"')+1:-1]
+	if name != deaccent(name):
+		line += ' "' + deaccent(name) + '"'
+	if name.startswith("The "):
+		line += ' "' + deaccent(name)[4:] + '"'
+	print(line)
